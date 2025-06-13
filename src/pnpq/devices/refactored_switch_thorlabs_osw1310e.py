9cfcf55b72a74461c9f53c24e29f5df8231a7361
remonnnokawa <oyihud3062@keio.jp>
2025-06-13 17:14:46 +0900

    Apply suggestions from code review
    Co-authored-by: Andrew Todd <at@auspicacious.org>

--- src/pnpq/devices/refactored_switch_thorlabs_osw1310e.py
+++ src/pnpq/devices/refactored_switch_thorlabs_osw1310e.py
@@ -12,15 +12,11 @@ from serial import Serial
 
 
 class State(Enum):
-    BAR = 1  # this might still trigger the pylint error
+    BAR = 1
     CROSS = 2
 
 
 class AbstractOpticalSwitchThorlabs1310E(ABC):
-    @abstractmethod
-    def connect(self) -> None:
-        """Open a new connection."""
-
     @abstractmethod
     def set_state(self, state: State) -> None:
         """Set the switch to the specified state."""
@@ -28,35 +24,62 @@ class AbstractOpticalSwitchThorlabs1310E(ABC):
 
 @dataclass(frozen=True, kw_only=True)
 class OpticalSwitchThorlabs1310E(AbstractOpticalSwitchThorlabs1310E):
+    # Required
+    serial_number: str
 
+    # Serial connection parameters
+    baudrate: int = field(default=115200)
+    bytesize: int = field(default=serial.EIGHTBITS)
+    exclusive: bool = field(default=True)
+    parity: str = field(default=serial.PARITY_NONE)
+    rtscts: bool = field(default=True)
+    stopbits: int = field(default=serial.STOPBITS_ONE)
+    timeout: None | int = field(
+        default=None  # None means wait forever, until the requested number of bytes are received
+    )
+
+    connection: Serial = field(init=False)
     def __post_init__(
         self,
-        serial_port: str | None = None,
-        serial_number: str | None = None,
     ) -> None:
-        self.conn = Serial()
-        self.conn.baudrate = 115200
-        self.conn.bytesize = 8
-        self.conn.parity = "N"
-        self.conn.rtscts = True
+        # These devices tend to take a few seconds to start up, and
+        # this library tends to be used as part of services that start
+        # automatically on computer boot. For safety, wait here before
+        # continuing initialization.
+        time.sleep(1)
 
-        self.port = serial_port
-        self.conn.port = self.port
-        self.device_sn = serial_number
-
-        find_port = False
-        if self.device_sn is not None:
-            available_ports = serial.tools.list_ports.comports()
-            for ports in available_ports:
-                if ports.serial_number == self.device_sn:
-                    self.conn.port = ports.device
-                    find_port = True
-                    break
-            if not find_port:
-                raise ValueError("Cannot find Switch by serial_number (FTDI_SN)")
-
-    def connect(self) -> None:
-        self.conn.open()
+        port_found = False
+        port = None
+        for port in serial.tools.list_ports.comports():
+            if port.serial_number == self.serial_number:
+                port_found = True
+                break
+        if not port_found:
+            raise ValueError(
+                f"Serial number {self.serial_number} could not be found, failing intialization."
+            )
+            
+        object.__setattr__(
+            self,
+            "connection",
+            Serial(
+                baudrate=self.baudrate,
+                bytesize=self.bytesize,
+                exclusive=self.exclusive,
+                parity=self.parity,
+                port=port.device,
+                rtscts=self.rtscts,
+                stopbits=self.stopbits,
+                timeout=self.timeout,
+            ),
+        )
+        
+        # Remove anything that might be left over in the buffer from
+        # previous runs
+        self.connection.flush()
+        time.sleep(1)
+        self.connection.reset_input_buffer()
+        self.connection.reset_output_buffer()
 
     def set_state(self, state: State) -> None:
         if state == State.BAR:
